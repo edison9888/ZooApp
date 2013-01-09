cacea004bc90879766ce220af2e52b39ce842970
AndreaGer <missgerlach@googlemail.com>
2013-01-09 02:53:49 +0100

    animal model changed, AnimalDetailView pagecontrol started
    filenames changed on some files (AG in front)
    Animal variables changed

---
+++ ZooApp/Model/AGPoi.h
@@ -0,0 +1,13 @@
+//
+//  AGPoi.h
+//  ZooApp
+//
+//  Created by Andrea Gerlach on 09.01.13.
+//  Copyright (c) 2013 de.andreagerlach. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AGPoi : NSObject
+
+@end
---
+++ ZooApp/Model/AGPoi.m
@@ -0,0 +1,13 @@
+//
+//  AGPoi.m
+//  ZooApp
+//
+//  Created by Andrea Gerlach on 09.01.13.
+//  Copyright (c) 2013 de.andreagerlach. All rights reserved.
+//
+
+#import "AGPoi.h"
+
+@implementation AGPoi
+
+@end
